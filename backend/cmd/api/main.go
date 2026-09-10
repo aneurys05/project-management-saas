f042d940142f7c9001d30fb0354eae0af8803c80
Aneurys Anglero <aneurys2090@gmail.com>
2026-09-09 20:40:59 -0400

    New version

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/aneurys05/project-management-saas/internal/database"
+	"github.com/aneurys05/project-management-saas/internal/user"
 )
 
 func main() {
@@ -22,8 +23,14 @@ func main() {
 	}
 	defer db.Close()
 
+	// User dependencies
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
+	userHandler := user.NewHandler(userService)
+
 	mux := http.NewServeMux()
 
+	// Health check
 	mux.HandleFunc("GET /api/v1/health", func(w http.ResponseWriter, r *http.Request) {
 		response := map[string]string{
 			"status": "ok",
@@ -36,6 +43,9 @@ func main() {
 			return
 		}
 	})
+
+	// Authentication
+	mux.HandleFunc("POST /api/v1/auth/register", userHandler.Register)
 
 	server := &http.Server{
 		Addr:    ":8080",
